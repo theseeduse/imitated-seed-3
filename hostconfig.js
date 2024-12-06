c88292e26bb2eacace5649a78e821aef19985cf7
theseeduse <qwert304411@naver.com>
2024-12-06 17:51:20 +0900

    Update hostconfig.js

--- hostconfig.js
+++ hostconfig.js
@@ -1,13 +1,13 @@
 const hostconfig = require('./config.json');
 
 if(hostconfig.theseed_version === undefined)
-	hostconfig.theseed_version = process.env.THESEED_VERSION || '4.12.0';
+	hostconfig.theseed_version = process.env.THESEED_VERSION || '4.25.0';
 
 if(hostconfig.host === undefined)
 	hostconfig.host = process.env.HOST || '0.0.0.0';
 
 if(hostconfig.port === undefined)
-	hostconfig.port = process.env.PORT || '8000';
+	hostconfig.port = process.env.PORT || '80';
 
 if(hostconfig.disable_file_server === undefined)
 	hostconfig.disable_file_server = !!Number(process.env.DISABLE_FILE_SERVER);
